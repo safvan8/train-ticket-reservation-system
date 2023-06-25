b823e2215d094b8e0e6f6b51a620d1a951a50432
Safvan <safvanpmcz@gmail.com>
2023-06-25 10:32:35 +0530

    initial commit

---
+++ src/main/java/com/safvan/TrainTicketReservationSystemSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.safvan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTicketReservationSystemSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainTicketReservationSystemSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safvan/TrainTicketReservationSystemSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safvan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketReservationSystemSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
