3640eb96149e79469baf584525836ed42c644d80
Safvan <safvanpmcz@gmail.com>
2023-08-31 11:18:29 +0530

    upgraded password encoding by adding  DelegatingPasswordEncoder-  helps in encoding password using latest technologies, also added a prefix in password, indictaing the underlaying password encoder alogorithm

--- src/main/java/com/safvan/config/AppConfig.java
+++ src/main/java/com/safvan/config/AppConfig.java
@@ -2,13 +2,14 @@ package com.safvan.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
 
 	@Bean
-	public BCryptPasswordEncoder createBCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
+	public PasswordEncoder createBCryptPasswordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 }
