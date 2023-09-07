36545a135709cc5fd93d1e3d1ab6603859040a28
Safvan <safvanpmcz@gmail.com>
2023-09-07 18:21:26 +0530

    trainServiceImpl test cases, 3 completed

--- src/test/java/com/safvan/service/mvc/impl/TrainServiceImplTest.java
+++ src/test/java/com/safvan/service/mvc/impl/TrainServiceImplTest.java
@@ -1,10 +1,12 @@
 package com.safvan.service.mvc.impl;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -38,6 +40,43 @@ public class TrainServiceImplTest {
 
 		// Assert
 		assertEquals(expectedTrains.size(), actualTrains.size());
+	}
+
+	@Test
+	@DisplayName("Given - All Trains Available, When - Get All Trains is Called, Then -  A List of trains is returned")
+	public void testGetAllTrainsWhenAllTrainsAvailable() {
+		// Arrange
+
+		Train train1 = new Train(1l, "TrainName1", "Bengaluru", "Kerala", 12, 50.0);
+		Train train2 = new Train(1l, "TrainName2", "Bengaluru", "Kerala", 122, 502.0);
+		Train train3 = new Train(1l, "TrainName3", "Bengaluru", "Kerala", 1242, 520.0);
+
+		List<Train> expectedTrains = List.of(train1, train2, train3);
+
+		given(trainRepository.findAll()).willReturn(expectedTrains);
+
+		// Act
+		List<Train> actualTrains = trainService.getAllTrains();
+
+		// Assert
+		assertEquals(expectedTrains.size(), actualTrains.size());
+	}
+
+	@Test
+	@DisplayName("Given - train Number, When - getTrainByNumber is called, Then - train with the specific Id is returned")
+	public void testGetTrainByNumber() {
+		// Arrange
+		Long trainNo = 10L;
+
+		Train expectedTrain = new Train(10L, "TrainName", "Bengaluru", "Kerala", 1242, 520.0);
+
+		given(trainRepository.findById(trainNo)).willReturn(Optional.of(expectedTrain)); // Return an Optional.
+
+		// Act
+		Train actualTrain = trainService.getTrainByNumber(trainNo);
+
+		// Assert
+		assertThat(actualTrain).isEqualTo(expectedTrain); // Use assertThat for object comparison.
 	}
 
 }
